4f35c278e83d04d81f8d6fcb33c3282ccac6ad4c
bayro.monsalve@4bits.cl <bayro.monsalve@4bits.cl>
2013-06-02 17:04:46 -0400

    Habilita log.

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontEnd', 'dev', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontEnd', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
