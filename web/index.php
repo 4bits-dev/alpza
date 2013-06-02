fb8ccdaf53abe49c445b084e72a0e333bb67ea75
bayro.monsalve@4bits.cl <bayro.monsalve@4bits.cl>
2013-06-02 17:08:36 -0400

    Se retorna a productivo

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontEnd', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontEnd', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
